21c52e0f9f74f73e395119a01871f4510c715115
Dru Jensen <drujensen@gmail.com>
2021-07-24 14:09:10 -0700

    fix: working version

--- eatermon.s
+++ eatermon.s
@@ -30,9 +30,8 @@ KBDDR           =       $6003       ; Keyboard Data Direction Register A
 PCR             =       $600C       ; Peripheral Control Register
 IFR             =       $600D       ; Interrupt Flag Register
 IER             =       $600E       ; Ineterrup Enable Register
-E               =       %01000000   ; Display Enable Bit
-RW              =       %00100000   ; Display RW Flag
-RS              =       %00010000   ; Display Register Select
+
+E               =       %10000000   ; Display triggered using bit 0 rising edge
 
 ;-------------------------------------------------------------------------
 ;  Constants
@@ -66,25 +65,14 @@ reset:
                 lda     #%10000010  ; enable CA1 interrupt
                 sta     IER
 
-; The following is specific initialization of the LCD Display and not found in the
-; original WozMon source code.
-
-                jsr     lcd_init
-                lda     #%00101000  ; Set 4-bit mode; 2-line display; 5x8 font
-                jsr     lcd_inst
-                lda     #%00001111  ; Display on; cursor on; blink on
-                jsr     lcd_inst
-                lda     #%00000110  ; Increment and shift cursor; don't shift display
-                jsr     lcd_inst
-                lda     #%00000001  ; Clear display
-                jsr     lcd_inst
-
 ; The program has been modified to use the IRQ instead of the 7tbh bit of the keybard.
 ; This allows support for 8bit ASCII codes from the keyboard.  We will display the
 ; prompt and loop forever.
 
+                jsr     delay
+                jsr     delay
                 lda     #PROMPT
-                jsr     lcd_echo
+                jsr     echo
 
 loop:
                 jmp     loop
@@ -97,44 +85,32 @@ loop:
 nextchar:
                 lda     KBD
                 sta     IN,Y
+                jsr     echo
+                cmp     #CR
+                beq     enter
                 cmp     #BS
                 beq     backspace
                 cmp     #ESC
                 beq     escape
-                cmp     #CR
-                beq     enter
-                jsr     lcd_echo
                 iny
                 rts
 
 backspace:
-                lda     #%00000100  ; Change display to decrement mode
-                jsr     lcd_inst
-                lda     #SPACE      ; Clear the previous display
-                jsr     lcd_echo
-                lda     #%00000110  ; Change display back to increment mode
-                jsr     lcd_inst
                 lda     #$00        ; clear value from buffer
                 sta     IN,Y
                 dey                 ; Decrement the buffer pointer
                 rts
 
 escape:
-                lda     #%00000001  ; Clear display
-                jsr     lcd_inst
-                lda     #PROMPT     ; Print promt character
-                jsr     lcd_echo    ; Output it.
                 ldy     #$00        ; Reset the buffer pointer
+                lda     #PROMPT     ; Print promt character
+                jsr     echo        ; Output it.
                 rts
 
 enter:
-                jsr     lcd_scroll  ; Scroll to the next line
-
-; Line received, now let's parse it
-
-                ldy     #$ff
-                lda     #$00
-                tax
+                ldy     #$ff        ; Reset text index
+                lda     #$00        ; Default mode is XAM
+                tax                 ; X=0
 
 setstor:
                 asl                 ; Leaves $7B if setting STOR mode
@@ -149,7 +125,7 @@ nextitem:
                 lda     IN,Y        ; Get character
                 cmp     #CR
                 bne     notdone
-                jsr     lcd_home    ; We're done if it's CR!
+                jsr     done        ; We're done if it's CR!
                 rts
 
 notdone:
@@ -237,17 +213,18 @@ setadr:
 
 nxtprnt:
                 bne     prdata       ; NE means no address to print
-                jsr     lcd_scroll   ; Print newline
+                lda     #CR
+                jsr     echo         ; Print newline
                 lda     XAMH         ; Output high-order byte of address
                 jsr     prbyte
                 lda     XAML         ; Output low-order byte of address
                 jsr     prbyte
                 lda     #':'         ; Print colon
-                jsr     lcd_echo
+                jsr     echo
 
 prdata:
                 lda     #SPACE        ; Print space
-                jsr     lcd_echo
+                jsr     echo
                 lda     (XAML,X)     ; Get data from address (X=0)
                 jsr     prbyte       ; Output it in hex format
 
@@ -291,113 +268,51 @@ prhex:
                 and     #%00001111   ; Mast LSD for hex print
                 ora     #'0'         ; Add "0"
                 cmp     #'9'+1       ; Is it a decimal digit?
-                bcc     lcd_echo     ; Yes! output it
+                bcc     echo         ; Yes! output it
                 adc     #6           ; Add offset for letter A-F
-                jsr     lcd_echo     ; Print it
-                rts                  ;
+
+; Fall through to print routine
 
 ;-------------------------------------------------------------------------
-;  Subroutine to print a character to the terminal.  Modified to support
-;  LCD Display.
+;  Subroutine to print a character to the terminal
 ;-------------------------------------------------------------------------
-lcd_init:
-                lda     #%00000010    ; Set 4-bit mode
+
+echo:
+                sta     DSP          ; Output character.
+                ora     #E           ; Set E bit
                 sta     DSP
-                ora     #E            ; Set Enable bit
+                eor     #E           ; Clear E bit
                 sta     DSP
-                eor     #E            ; Clear Enable bit
-                sta     DSP
-                jsr     lcd_wait      ; Wait for display
+                jsr     delay
                 rts
 
-lcd_inst:
-                pha                   ; Push A for LSD later
-                lsr
-                lsr                   ; MSD to LSD position
-                lsr
-                lsr                   ; Send high 4 bits
-                sta     DSP
-                ora     #E            ; Set Enable bit
-                sta     DSP
-                eor     #E            ; Clear Enable bit
-                sta     DSP
-                pla
-                and     #%00001111    ; Mask LSD
-                sta     DSP
-                ora     #E            ; Set Enable bit
-                sta     DSP
-                eor     #E            ; Clear Enable bit
-                sta     DSP
-                jsr     lcd_wait      ; Wait for Display
-                rts
-
-lcd_echo:
-                pha                   ; Push A for LSD later
-                lsr
-                lsr                   ; MSD to LSD position
-                lsr
-                lsr
-                ora     #RS           ; Set Register Select bit
-                sta     DSP
-                ora     #E            ; Set Enable bit
-                sta     DSP
-                eor     #E            ; Clear Enable bit
-                sta     DSP
-                pla
-                and     #%00001111    ; Mask LSD
-                ora     #RS           ; Set Register Select bit
-                sta     DSP
-                ora     #E            ; Set Enable bit
-                sta     DSP
-                eor     #E            ; Clear Enable bit
-                sta     DSP
-                jsr     lcd_wait      ; Wait for Display
-                rts
-
-lcd_scroll:
-                lda     #%10101000    ; put cursor on next line
-                jsr     lcd_inst
-                rts
-
-lcd_home:
-                lda     #%00000010    ; move the cursor to home
-                jsr     lcd_inst
-                lda     #PROMPT       ; Print the prompt
-                jsr     lcd_echo
-                rts
-
-lcd_wait:
+delay:
                 pha
-                lda     #%11110000    ; Set LCD register to input mode
-                sta     DSPDR
-lcd_busy:
-                lda     #RW           ; Set Read bit
-                sta     DSP
-                lda     #(RW | E)     ; Set Enable bit
-                sta     DSP
-                lda     DSP           ; Read high nibble
-                pha                   ; push on stack since it has the busy flag
-                lda     #RW
-                sta     DSP
-                lda     #(RW | E)
-                sta     DSP
-                lda     DSP           ; Read low nibble
-                pla                   ; Get high nibble off stack
-                and     #%00001000    ; Mask busy bit
-                bne     lcd_busy      ; Still busy.
-
-                lda     #RW
-                sta     DSP
-                lda     #%11111111    ; Set LCD register to output mode
-                sta     DSPDR
+                tax
+                pha
+                ldx    #$FF
+dloop:
+                dex
+                nop
+                nop
+                bne     dloop
+                pla
+                txa
                 pla
                 rts
 
-nmi:
+done:
+                lda     #CR
+                jsr     echo
+                lda     #PROMPT
+                jsr     echo
                 rti
 
 irq:
                 jsr     nextchar      ; key pressed!
+                rti
+
+nmi:
                 rti
 
 .segment "VECTORS"
